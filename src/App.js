a7be1a188bb2435f5d37f84d77a8969d02687f23
bishthub <bisht@Shubhams-MacBook-Pro.local>
2024-09-11 20:32:38 +0530

    iframe

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import pdfFile from './assets/deck.pdf';
 function App() {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <embed src={pdfFile} type='application/pdf' width='100%' height='100%' />
+      <iframe src={pdfFile} type='application/pdf' width='100%' height='100%' />
     </div>
   );
 }
